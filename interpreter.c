8edb73238a06053cf4c737dcd2ea8766a69c1a5b
Edgar Bonet <bonet@grenoble.cnrs.fr>
2016-11-29 14:07:12 +0100

    Implement 'start' and 'stop' commands
    The syntax for 'start' is 'start port_number, frequency', where
    port_number is 1 or 2 (for _COM1 and _COM2 respectively) and frequency
    is either 0 (for _NOTBEATING), 50 or 60.
    
    The 'stop' command takes no parameters.

--- interpreter.c
+++ interpreter.c
@@ -333,6 +333,68 @@ static int idn(void *client, unused(int cmd_data), parsed_command *cmd)
     return 0;
 }
 
+/* syntax: "start serial_port_number, frequency" */
+static int start(unused(void *client), unused(int cmd_data),
+        parsed_command *cmd)
+{
+    INITSTRUCTURE init;
+
+    /* Sanity check. */
+    assert(cmd->n_tok == 1);
+    if (cmd->query || cmd->suffix[0] != -1 || cmd->n_param != 2) {
+        push_error("Malformed start command");
+        return 1;
+    }
+
+    /* Parse parameters. */
+    int port = atoi(cmd->param[0]);
+    if (port == 1)
+        init.Com = _COM1;
+    else if (port == 2)
+        init.Com = _COM2;
+    else {
+        push_error("Invalid serial port number");
+        return 1;
+    }
+    int freq = atoi(cmd->param[1]);
+    if (freq == 0)
+        init.Frequency = _NOTBEATING;
+    else if (freq == 50)
+        init.Frequency = _50HZ;
+    else if (freq == 60)
+        init.Frequency = _60HZ;
+    else {
+        push_error("Invalid frequency");
+        return 1;
+    }
+
+    /* Proceed to start the TRMC2. */
+    int ret = StartTRMC(&init);
+    if (ret) {
+        push_error(const_name(ret, error_codes));
+        return 1;
+    }
+    return 0;
+}
+
+static int stop(unused(void *client), unused(int cmd_data),
+        parsed_command *cmd)
+{
+    /* Sanity check. */
+    assert(cmd->n_tok == 1);
+    if (cmd->query || cmd->suffix[0] != -1 || cmd->n_param != 0) {
+        push_error("Malformed stop command");
+        return 1;
+    }
+
+    int ret = StopTRMC();
+    if (ret) {
+        push_error(const_name(ret, error_codes));
+        return 1;
+    }
+    return 0;
+}
+
 static int quit(unused(void *client),
         unused(int cmd_data), parsed_command *cmd)
 {
@@ -353,6 +415,8 @@ static int quit(unused(void *client),
 
 const syntax_tree trmc2_syntax[] = {
     {"*idn", idn, 0, NULL},
+    {"start", start, 0, NULL},
+    {"stop", stop, 0, NULL},
     {"board", NULL, 0, (syntax_tree[]) {
         {"count", get_number, nb_boards, NULL},
         {"type", board_handler, b_type, NULL},
